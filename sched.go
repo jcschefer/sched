7cd839c54e33427d1ce3b2bcf6b0fb82e8574d77
Jack Schefer <jack.schefer9@gmail.com>
2017-01-24 18:31:01 -0500

    export to helper functions and fix the tabbing for long block names

--- sched.go
+++ sched.go
@@ -51,31 +51,7 @@ type Response struct {
 //
 func main() {
    if len(os.Args) > 1 && os.Args[1] == "week" {
-      // show the order of days in the week
-      fmt.Println("\nRemainder of the week's schedule.\n")
-      _, this_week := time.Now().ISOWeek()
-      if time.Now().Weekday() == 6 {  // if it's saturday, display the next week anyway 
-         this_week = (this_week + 1) % 53
-      }
-      //
-      var i int = 1
-      next_name, next_date := parseJSON(1)
-      t, err := time.Parse("2006-01-02", next_date)
-      check(err)
-      _, week := t.ISOWeek()
-      for week == this_week {
-         fmt.Printf("%s: %s\n", t.Weekday(), next_name)
-         i = i + 1
-         next_name, next_date = parseJSON(i)
-         t, err = time.Parse("2006-01-02", next_date)
-         check(err)
-         _, week = t.ISOWeek()
-         // so that next_name isn't unused
-         foo := strings.Index(next_name, "foo")
-         if foo > -1 {
-            fmt.Println("name contains foo!")
-         }
-      }
+      show_weekly_schedule()
       //
    } else {
       //
@@ -83,25 +59,8 @@ func main() {
       //
       // 1. Get and print out the current date and time.
       //
-      fmt.Println()
       now := time.Now()
-      var tsuffix string
-      var zero string = ""
-      fmt.Printf("%v, %v %v\n", now.Weekday(), now.Month(), now.Day())
-      if now.Hour() / 12 == 0 {
-         tsuffix = "am"
-      } else {
-         tsuffix = "pm"
-      }
-      if now.Minute() < 10 {
-         zero = "0"
-      }
-      hr := now.Hour() % 12
-      if hr == 0 {
-         hr = 12
-      }
-      fmt.Printf("Time: %v:%s%v %s\n", hr, zero, now.Minute(), tsuffix)
-      fmt.Println()
+      print_current_time( now )
       //
       // 2. Get the schedule data from Ion
       url_params := ""
@@ -127,9 +86,16 @@ func main() {
       title = strings.Replace(title, "<br>", " ", -1)
       fmt.Println(title)
       //
+      max_name_length := 0
+      for _,b := range data.Results[0].DayType.Blocks {
+         if len(strings.Replace( b.Name + ":", "<br>", " ", -1)) > max_name_length {
+            max_name_length = len(strings.Replace( b.Name + ":", "<br>", " ", -1))
+         }
+      }
+      //
       for _, b := range data.Results[0].DayType.Blocks {
          name := strings.Replace( b.Name + ":", "<br>", " ", -1)
-         for i := len(name); i < 14; i++ {
+         for i := len(name); i < max_name_length; i++ {
             name += " "
          }
          //
@@ -166,6 +132,59 @@ func main() {
 ///////////////////////////////////////////////////////////
 //
 //  HELPER METHODS
+//
+///////////////////////////////////////////////////////////
+//
+func show_weekly_schedule() {
+   // show the order of days in the week
+   fmt.Println("\nRemainder of the week's schedule.\n")
+   _, this_week := time.Now().ISOWeek()
+   if time.Now().Weekday() == 6 {  // if it's saturday, display the next week anyway 
+      this_week = (this_week + 1) % 53
+   }
+   //
+   var i int = 1
+   next_name, next_date := parseJSON(1)
+   t, err := time.Parse("2006-01-02", next_date)
+   check(err)
+   _, week := t.ISOWeek()
+   for week == this_week {
+      fmt.Printf("%s: %s\n", t.Weekday(), next_name)
+      i = i + 1
+      next_name, next_date = parseJSON(i)
+      t, err = time.Parse("2006-01-02", next_date)
+      check(err)
+      _, week = t.ISOWeek()
+      // so that next_name isn't unused
+      foo := strings.Index(next_name, "foo")
+      if foo > -1 {
+         fmt.Println("name contains foo!")
+      }
+   }
+}
+//
+///////////////////////////////////////////////////////////
+//
+func print_current_time( now time.Time ) {
+   fmt.Println()
+   var tsuffix string
+   var zero string = ""
+   fmt.Printf("%v, %v %v\n", now.Weekday(), now.Month(), now.Day())
+   if now.Hour() / 12 == 0 {
+      tsuffix = "am"
+   } else {
+      tsuffix = "pm"
+   }
+   if now.Minute() < 10 {
+      zero = "0"
+   }
+   hr := now.Hour() % 12
+   if hr == 0 {
+      hr = 12
+   }
+   fmt.Printf("Time: %v:%s%v %s\n", hr, zero, now.Minute(), tsuffix)
+   fmt.Println()
+}
 //
 ///////////////////////////////////////////////////////////
 //
